f22ddf9ddb58ad0328a424cc9b3f3081b97da46e
NaNdalal-dev <dnandalal7@gmail.com>
2021-07-29 08:54:59 +0530

    Commit 2

--- Blog-App/Blog/blog_articles/views.py
+++ Blog-App/Blog/blog_articles/views.py
@@ -6,4 +6,5 @@ def home(request):
 
 def blog_in_detail(request, link):
 	blog =  Blog.objects.get(id=link)
-	return render(request,"blog.html",{'blog':blog})
+	blg_field = blog.BlogField.split('\n')
+	return render(request,"blog.html",{'blog':blog,'blg_field':blg_field,'title':blog.Title})
